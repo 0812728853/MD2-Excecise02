c1914a31e063a4cc61af5324cd6a958324880d08
0812728853 <tuankai100@gmail.com>
2023-11-20 09:47:53 +0700

    upload Excecise02

---
+++ src/BaiTap/HomePage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Header from './Header'
+import Menu from './Menu'
+import TitleProducts from './TitleProducts'
+import ListProducts from './ListProducts'
+import Footer from './Footer'
+
+function HomePage() {
+  return (
+    <>
+  {/* Header  */}
+  <Header/>
+  {/* End header area */}
+  {/* End site branding area */}
+  <Menu/>
+  {/* End mainmenu area */}
+  <TitleProducts/>
+  {/* List product */}
+  <ListProducts/>
+  {/* Footer area */}
+  <Footer/>
+  {/* Latest jQuery form server */}
+  {/* Bootstrap JS form CDN */}
+  {/* jQuery sticky menu */}
+  {/* jQuery easing */}
+  {/* Main Script */}
+</>
+
+  )
+}
+
+export default HomePage
